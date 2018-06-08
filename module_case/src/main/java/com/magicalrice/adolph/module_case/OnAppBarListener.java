9a6504a8e959cfaca5851420428e53340b57dff9
adolph <adolphJane@users.noreply.github.com>
2018-06-08 18:42:43 +0800

    commit

---
+++ module_case/src/main/java/com/magicalrice/adolph/module_case/OnAppBarListener.java
@@ -0,0 +1,12 @@
+package com.magicalrice.adolph.module_case;
+
+import android.support.annotation.IdRes;
+
+/**
+ * Created by Adolph on 2018/6/6.
+ */
+
+public interface OnAppBarListener {
+    void onTitleBarItemClick(@IdRes int id);
+    void initEnd(int miniTopHeight);
+}
